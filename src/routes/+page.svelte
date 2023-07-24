7099ad33691734912ca9f6f6e0bda8a9e0607e07
João Miranda <joao.miranda@comparaja.pt>
2023-07-24 23:56:24 +0100

    added hero and start of carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,42 @@
-<h1>Index</h1>
+<script>
+	import Carousel from '$lib/components/Carousel.svelte';
+	import { CldImage } from 'svelte-cloudinary';
+</script>
+
+<section class="container mx-auto px-4 mt-14">
+	<div class="flex justify-center gap-x-14 xl:gap-x-28">
+		<div class="max-w-lg">
+			<h1
+				class="text-transparent bg-gradient-to-r from-[#17742F] to-[#E0E482] bg-clip-text leading-normal font-bold inline-block text-5xl"
+			>
+				Faça a diferença e reporte a poluição
+			</h1>
+
+			<h2 class="mt-6 text-dark-green text-2xl">
+				Seja parte da mudança e <span class="font-bold"
+					>ajude a reportar e monitorizar a poluição</span
+				> em todo o território nacional.
+			</h2>
+
+			<p class="mt-8 max-w-[430px]">
+				<span class="font-bold">
+					Com a App EcoReport, pode contribuir para um ambiente mais saudável e preservar a beleza
+					natural do nosso país.
+				</span>
+				<br /><br />
+				Junte-se a nós na missão de um <span class="font-bold">Portugal limpo e sustentável</span>
+			</p>
+
+			<button
+				class="mt-8 bg-gradient-to-r from-[#17742F] to-[#E0E482] text-white text-xl py-2 px-8 rounded-[34px]"
+				type="button">Transfira a aplicação</button
+			>
+		</div>
+
+		<CldImage width={538} height={538} src="hero_bg_we3avh" alt="Hero" sizes="100vw" />
+	</div>
+</section>
+
+<section class="container mx-auto px-4">
+	<Carousel />
+</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}', 'node_modules/preline/dist/*.js'],
 	theme: {
 		extend: {
+			boxShadow: {
+				s: '0px 2px 10px 0px rgba(23, 116, 47, 0.10)'
+			},
 			colors: {
 				'dark-green': '#17742f',
 				green: '#E0E482',
