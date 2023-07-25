b6273464edb07eb2df06404fa38ae8ef939b1a35
João Miranda <joao.miranda@comparaja.pt>
2023-07-25 23:20:56 +0100

    added sitemap.xml and robots.txt

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,42 @@
+export async function GET() {
+	const headers = {
+		'Cache-Control': 'max-age=0, s-maxage=3600',
+		'Content-Type': 'application/xml'
+	};
+
+	const pages = ['app', 'partnerships', 'reports', 'sponsors'];
+	const website = 'https://www.ecoreport.pt';
+
+	return new Response(
+		`
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+          xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xhtml="https://www.w3.org/1999/xhtml"
+          xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+          xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+          xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+          xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+        <url>
+          <loc>${website}</loc>
+          <changefreq>daily</changefreq>
+          <priority>0.6</priority>
+        </url>
+        ${pages.map(
+					(page) => `
+        <url>
+          <loc>${website}/${page}</loc>
+          <changefreq>daily</changefreq>
+          <priority>0.6</priority>
+        </url>
+        `
+				)}
+      </urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
+}
