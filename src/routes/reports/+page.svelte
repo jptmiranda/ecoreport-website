896d863befed24f9705ceabaa1578a8acd93a957
João Miranda <joao.t.m@proton.me>
2023-08-19 17:05:52 +0100

    subscribe only after page loads

--- src/routes/reports/+page.svelte
+++ src/routes/reports/+page.svelte
@@ -2,35 +2,38 @@
 	import Counter from '$lib/components/reports/Counter.svelte';
 	import MonthlyGraph from '$lib/components/reports/MonthlyGraph.svelte';
 	import type { MonthlyReport } from '$lib/types';
+	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	data.supabase
-		.channel('schema-db-changes')
-		.on(
-			'postgres_changes',
-			{
-				event: 'INSERT',
-				schema: 'public'
-			},
-			async (payload) => {
-				if (payload.table === 'reports') {
-					const { data: count } = await data.supabase.rpc('count_reports');
-					const { data: monthlyCount } = (await data.supabase
-						.from('monthly_reports_count')
-						.select()) as {
-						data: MonthlyReport[];
-					};
+	onMount(() => {
+		data.supabase
+			.channel('schema-db-changes')
+			.on(
+				'postgres_changes',
+				{
+					event: 'INSERT',
+					schema: 'public'
+				},
+				async (payload) => {
+					if (payload.table === 'reports') {
+						const { data: count } = await data.supabase.rpc('count_reports');
+						const { data: monthlyCount } = (await data.supabase
+							.from('monthly_reports_count')
+							.select()) as {
+							data: MonthlyReport[];
+						};
 
-					data.reportsCount.total = count.total;
-					data.reportsCount.unresolved = count.unresolved;
-					data.reportsCount.resolved = count.resolved;
-					data.monthlyCount = monthlyCount;
+						data.reportsCount.total = count.total;
+						data.reportsCount.unresolved = count.unresolved;
+						data.reportsCount.resolved = count.resolved;
+						data.monthlyCount = monthlyCount;
+					}
 				}
-			}
-		)
-		.subscribe();
+			)
+			.subscribe();
+	});
 </script>
 
 <section class="container mx-auto px-4 lg:px-8 mt-8 lg:mt-12">
